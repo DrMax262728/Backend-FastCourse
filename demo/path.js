ead8636e7699a1d9f539d02738b63ee925e1766c
Max Halynskyi <maxhalynskyi@gmail.com>
2020-02-25 18:49:38 +0200

    update v.1.0

--- demo/path.js
+++ demo/path.js
@@ -1,3 +1,21 @@
 const path = require('path');
 
-console.log('path: ', path);
+// system files
+console.log('FileName:', __filename);
+console.log('DirName:', __dirname);
+
+// --- path ---
+// name
+const baseName = path.basename(__filename);
+// extension
+const extName = path.extname('/home/cultum/Desktop/Backend/Backend-FastCourse/demo/test.tsx');
+// parse (object)
+const parse = path.parse(__filename);
+// join
+const join = path.join(__dirname, 'papka', 'index.html');
+
+
+
+console.log('baseName: ', baseName);
+console.log('parse: ', parse.name);
+console.log('join: ', join);
