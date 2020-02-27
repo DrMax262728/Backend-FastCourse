9934bf4c3b987ae797c69229f68ed8cdad5cd20a
Max Halynskyi <maxhalynskyi@gmail.com>
2020-02-27 17:58:26 +0200

    update v.1.3

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((request, response) => {
-  response.end('<h1>Hello Node.js!!!</h1>');
+
+  // if (request.url === '/') {
+  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+  //     if (err) {
+  //       throw err
+  //     }
+  //
+  //     response.writeHead(200, {
+  //       'Content-type': 'text/html'
+  //     });
+  //
+  //     response.end(data);
+  //   })
+  // } else if (request.url === '/contact') {
+  //   fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+  //     if (err) {
+  //       throw err
+  //     }
+  //
+  //     response.writeHead(200, {
+  //       'Content-type': 'text/html'
+  //     });
+  //
+  //     response.end(data);
+  //   })
+  // }
+
+  let filePath = path.join(__dirname, 'pubic', request.url === '/' ? 'index.html' : request.url );
+
+  console.log(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if(err) {
+      fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+        if (err) {
+          response.writeHead(500);
+          response.end('Error!');
+        } else {
+          response.writeHead(200, {
+            'Content-type': 'text/html'
+          })
+        }
+      })
+    }
+  });
+
+  response.end();
 });
 
 server.listen(3131, () => {
